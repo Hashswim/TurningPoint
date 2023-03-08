3a3736fa294df01c7000953bde8a33296e33cf63
hashswim <ss991117@naver.com>
2023-03-08 15:50:10 +0900

    feat: stock 데이터 모델 기본 구현

---
+++ Stock.swift
@@ -0,0 +1,20 @@
+//
+//  Stock.swift
+//  autoTradingApp
+//
+//  Created by 서수영 on 2023/03/08.
+//
+
+import Foundation
+
+class Stock {
+    let name: String
+    var price: Double
+
+    init(name: String, price: Double) {
+        self.name = name
+        self.price = price
+    }
+
+
+}
--- autoTradingApp/View/SignUp/SignUpIsUserView.swift
+++ autoTradingApp/View/SignUp/SignUpIsUserView.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class SignUpIsUserView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let guideLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        label.text = "대신증권\n고객이신가요?"
+        label.textColor = .systemGray
+        return label
+    }()
+
+    private let
+
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 
 }
