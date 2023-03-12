ac2a008122170de628dd7577b38b5e68935c24c4
hashswim <ss991117@naver.com>
2023-03-12 21:10:27 +0900

    feat: Main Home tab 기본 컴포넌트 추가

--- autoTradingApp/Controller/MainTabBarController/TabBarItem/MainHomeTabControllerCollectionViewController.swift
+++ autoTradingApp/Controller/MainTabBarController/TabBarItem/MainHomeTabControllerCollectionViewController.swift
@@ -15,27 +15,94 @@ private enum Section: Hashable {
 
 class MainHomeTabControllerCollectionViewController: UIViewController {
 
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이수림님"
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2023.03.09"
+        label.font = .systemFont(ofSize: 10)
+
+        return label
+    }()
+
+    private let refreshButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "arrow.2.circlepath"), for: .normal)
+        return btn
+    }()
+
+    lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, timeLabel, refreshButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+
+        return stackView
+    }()
+
+    private let segmentedControl = mainTabSegmentedControl(items: ["내주식", "즐겨찾기", "트레이딩 주식"])
+
+    private let headerStcokLabel: UILabel = {
+        let label = UILabel()
+        label.text = "주식명"
+
+        return label
+    }()
+
+    private let headerChartLabel: UILabel = {
+        let label = UILabel()
+        label.text = "차트"
+
+        return label
+    }()
+
+    private let headerPriceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재가"
+
+        return label
+    }()
+
+    lazy var headerLabelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [headerStcokLabel, headerChartLabel, headerPriceLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+
+        return stackView
+    }()
+
     private var dataSource: UICollectionViewDiffableDataSource<Section, Stock>! = nil
     private var collectionView: UICollectionView! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "main tab"
+//        navigationItem.title = "main tab"
         configureHierarchy()
         configureDataSource()
+        configureLayout()
     }
 }
 
 extension MainHomeTabControllerCollectionViewController {
     private func createLayout() -> UICollectionViewLayout {
         let config = UICollectionLayoutListConfiguration(appearance: .plain)
+//        config.headerMode = .supplementary
         return UICollectionViewCompositionalLayout.list(using: config)
     }
 }
 
 extension MainHomeTabControllerCollectionViewController {
     private func configureHierarchy() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        view.backgroundColor = .white
+        view.addSubview(containerStackView)
+        view.addSubview(segmentedControl)
+        view.addSubview(headerLabelStackView)
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(collectionView)
         collectionView.delegate = self
@@ -58,6 +125,34 @@ extension MainHomeTabControllerCollectionViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(Stock.all)
         dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+    private func configureLayout() {
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+
+            refreshButton.trailingAnchor.constraint(equalTo: containerStackView.trailingAnchor),
+
+            segmentedControl.topAnchor.constraint(equalTo: containerStackView.bottomAnchor, constant: 32),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            segmentedControl.heightAnchor.constraint(equalToConstant: 20),
+
+            headerLabelStackView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+            headerLabelStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            headerLabelStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            headerLabelStackView.heightAnchor.constraint(equalToConstant: 20),
+
+            collectionView.topAnchor.constraint(equalTo: headerLabelStackView.bottomAnchor, constant: 20),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
---
+++ autoTradingApp/View/MenuTab/MainTabView/mainTabSegmentedControl.swift
@@ -0,0 +1,33 @@
+//
+//  mainTabSegmentedControl.swift
+//  autoTradingApp
+//
+//  Created by 서수영 on 2023/03/12.
+//
+
+import UIKit
+
+class mainTabSegmentedControl: UISegmentedControl {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.removeBackgroundAndDivider()
+      }
+      override init(items: [Any]?) {
+        super.init(items: items)
+        self.removeBackgroundAndDivider()
+      }
+      required init?(coder: NSCoder) {
+        fatalError()
+      }
+
+      private func removeBackgroundAndDivider() {
+        let image = UIImage()
+        self.setBackgroundImage(image, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(image, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
+
+        self.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+      }
+
+}
