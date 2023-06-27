0e2d15b887112a09c676e0f9cc5bef46c82087e4
hashswim <ss991117@naver.com>
2023-06-27 18:16:50 +0900

    feat: Search Tab 디자인 적용

--- autoTradingApp/Controller/MainTabBarController/TabBarItem/Search/SearchViewController.swift
+++ autoTradingApp/Controller/MainTabBarController/TabBarItem/Search/SearchViewController.swift
@@ -7,6 +7,54 @@
 
 import UIKit
 
+class SearchViewCustomCell: UITableViewCell {
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = MySpecialColors.darkGray
+
+        return view
+    }()
+
+    let stockLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        layout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    func layout() {
+        self.addSubview(containerView)
+        containerView.addSubview(stockLabel)
+
+        self.backgroundColor = MySpecialColors.red
+
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 51),
+
+            containerView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            containerView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 2),
+            containerView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -2),
+            containerView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+
+            stockLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
+            stockLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 36),
+            stockLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            stockLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+        ])
+    }
+}
+
 class SearchViewController: UIViewController {
 
     private let tableView = UITableView()
@@ -37,17 +85,34 @@ class SearchViewController: UIViewController {
 
         searchController.searchResultsUpdater = self
         searchController.searchBar.placeholder = "Search Stock"
+        searchController.searchBar.setValue("취소", forKey: "cancelButtonText")
+        searchController.searchBar.searchTextField.backgroundColor = MySpecialColors.textGray
+        //        searchController.searchBar.tintColor = MySpecialColors.bgColor
+        //        searchController.searchBar.searchTextField.font =
         self.navigationItem.hidesSearchBarWhenScrolling = false
         self.navigationItem.searchController = searchController
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = MySpecialColors.bgColor
+
+        self.navigationItem.standardAppearance = appearance
+        self.navigationItem.scrollEdgeAppearance = appearance
+
+        searchController.searchBar.tintColor = .white
     }
 
     func setUpTableView() {
         self.tableView.delegate = self
         self.tableView.dataSource = self
+
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = MySpecialColors.bgColor
     }
 
     func configureUI() {
         view.addSubview(tableView)
+        view.backgroundColor = MySpecialColors.bgColor
     }
 
     func configureLayout() {
@@ -69,11 +134,12 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = SearchViewCustomCell()
+        cell.backgroundColor = MySpecialColors.bgColor
         if self.isFiltering {
-            cell.textLabel?.text = self.filteredArr[indexPath.row]
+            cell.stockLabel.attributedText = NSMutableAttributedString().medium(string: self.filteredArr[indexPath.row], fontSize: 15)
         } else {
-            cell.textLabel?.text = self.arr[indexPath.row]
+            cell.stockLabel.attributedText = NSMutableAttributedString().medium(string: self.arr[indexPath.row], fontSize: 15)
         }
         return cell
     }
